2a7378c5050ce95e9a174e0e77c5383662d2db44
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2016-08-22 13:47:55 -0300

    Create data_copy.py
    Matching the dataset and copying it to another file.

---
+++ data_copy.py
@@ -0,0 +1,38 @@
+import numpy as np
+import os
+
+my_data = np.loadtxt('/home/mldantas/Dropbox/Clustering/dataset_clustering_abs.csv', delimiter=',', dtype=str)
+
+mjd     = my_data[:, 1].astype(int)
+plate   = my_data[:, 0].astype(int)
+fiberid = my_data[:, 2].astype(int)
+
+filelist_path       = '/media/mldantas/SAMSUNG/LinuxBackupIAG/Documentos/Clustering/filelist.txt'
+spec_file_path      = '/media/mldantas/SAMSUNG/LinuxBackupIAG/Documentos/Clustering/DATABASE'
+spec_file_selection = '/home/mldantas/Documents/Clustering/DATASET_selected'
+
+
+filelist = np.loadtxt(filelist_path, dtype=str)
+
+indexes  = np.arange(plate.size)
+
+for spec_file in filelist:
+        basename = os.path.split(spec_file)[-1]     # Get filename
+        basename = os.path.splitext(basename)[0]    # Remove file extension
+
+        plates   = basename.split('.')[0]
+        mjds     = basename.split('.')[1]
+        fiberids = basename.split('.')[2]
+
+        plates   = int(plates)
+        mjds     = int(mjds)
+        fiberids = int(fiberids)
+
+        index = indexes[(plate == plates) * (mjd == mjds) * (fiberid == fiberids)]
+
+        if index.size is 0:
+                continue
+
+        selected_object = np.loadtxt(os.path.join(spec_file_path, spec_file))
+
+        np.savetxt(os.path.join(spec_file_selection, basename+'.txt'), selected_object, fmt="%d,%.4f,%.4f,%d")
