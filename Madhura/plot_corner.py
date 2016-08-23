540aed70d995214fa3c151448b889984c77752d8
“DrMud” <madhura.killedar.astro@gmail.com>
2016-08-23 19:51:10 +0200

    add 4000 angstrom break

--- Madhura/plot_corner.py
+++ Madhura/plot_corner.py
@@ -3,35 +3,31 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-#data_full = pd.read_csv('../data/learning_parameters.csv')
-data = pd.read_csv('Dataset/clean_data.csv')
+data = pd.read_csv('../Dataset/clean_data.csv')
 print data
 
-#clean
+# clean further
 toclean = ["H_beta", "OIII"]
 for feat in toclean:
     data = data[data[feat] > -900]
-print data
 
-#ratios
+# ratios
 data["log([OIII]/H_beta)"] = log(data["OIII"]/data["H_beta"])
 data["log([NII]/H_alpha)"] = log(data["NII"]/data["H_alpha"])
 print data
 
 # subset
-subset = ["EW_H_alpha", "log([OIII]/H_beta)", "log([NII]/H_alpha)"]
+subset = ["EW_H_alpha", "log([OIII]/H_beta)", "log([NII]/H_alpha)", "dn4000_synth"]
+#subset = ["EW_H_alpha", "log([OIII]/H_beta)", "log([NII]/H_alpha)"]
 data = data[subset]
+data = data[data["EW_H_alpha"]<60] # just for cleaner plotting
 print data
 
-#plot
-data = data[data["EW_H_alpha"]<60] # just for plotting
+# plot
 g = sns.PairGrid(data)
-#g = g.map(sns.kdeplot)
 g.map_offdiag(sns.kdeplot, n_levels=6)
 g.map_diag(sns.kdeplot, legend=False, shade=True)
-#ax = sns.pairplot(data, diag_kind="kde", diag_kws=dict(shade=True))
 plt.savefig("agn_cornerplot.png",format="png")
-#plt.show()
 
 
 # clustering for BPT
