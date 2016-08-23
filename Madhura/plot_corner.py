433a9029bf46452064b257eec2e7500861f76351
“DrMud” <madhura.killedar.astro@gmail.com>
2016-08-24 01:17:18 +0200

    only plot the 4 parameters"

--- Madhura/plot_corner.py
+++ Madhura/plot_corner.py
@@ -1,4 +1,4 @@
-from numpy import log10, float
+from numpy import log10
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -15,36 +15,16 @@ for feat in toclean:
 data["log10([OIII]/H_beta)"] = log10(data["OIII"]/data["H_beta"])
 data["log10([NII]/H_alpha)"] = log10(data["NII"]/data["H_alpha"])
 data["log10(EW Halpha)"]     = log10(data["EW_H_alpha"])
+data.rename(columns={"dn4000_synth":"4000 Angstrom break"}, inplace=True)
 
 # subset
-#subset = ["EW_H_alpha", "log10([OIII]/H_beta)", "log10([NII]/H_alpha)", "dn4000_synth"]
-#subset = ["EW_H_alpha", "log10([OIII]/H_beta)", "dn4000_synth"]
-subset = ["log10(EW Halpha)", "log10([OIII]/H_beta)", "dn4000_synth"]
-#subset = ["EW_H_alpha", "log10([OIII]/H_beta)", "log10([NII]/H_alpha)"]
+subset = ["log10(EW Halpha)", "log10([OIII]/H_beta)", "log10([NII]/H_alpha)", "4000 Angstrom break"]
 data = data[subset]
-#data = data[data["EW_H_alpha"]<60] # just for cleaner plotting
 print data
 
 # plot
-"""
 g = sns.PairGrid(data)
 g.map_offdiag(sns.kdeplot, n_levels=6)
 g.map_diag(sns.kdeplot, legend=False, shade=True)
 plt.savefig("agn_cornerplot.png",format="png")
-"""
 
-# clustering for 4
-from sklearn.cluster import KMeans
-km = KMeans(n_clusters=3, max_iter=300, n_init=12, init="random")
-km.fit(data)
-labels = km.predict(data)
-print labels.astype(float)
-xax = "dn4000_synth"
-yax = "log10(EW Halpha)"
-plt.figure(1)
-plt.scatter(data[xax], data[yax], c=labels.astype(float), )
-plt.title("KMeans clustering on %i dimensions"%(len(subset)))
-plt.xlabel(xax)
-plt.ylabel(yax)
-plt.savefig("cluster_KMeans.png",format="png")
-plt.show()
