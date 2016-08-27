529eb8c1ed9a6ee26f66617e0d3732bb5a6203d8
Rafael <rafael.2706@gmail.com>
2016-08-27 12:48:08 +0200

    csv file

--- Rafael_R_scripts/GMM_WHAN_BPT.R
+++ Rafael_R_scripts/GMM_WHAN_BPT.R
@@ -1,38 +1,22 @@
 library(e1071);require(mclust);library(RColorBrewer);require(ggthemes);
 require(ggpubr);require(ggplot2)
 
-AGN<- read.csv("/Users/rafael/Dropbox/artigos/Meusartigos/IAA-WGC/Github/AGN_unsupervised/script/clean_AGN_data.csv",header=T)
-AGN_high <- data.frame(log(AGN$NII/AGN$H_alpha,10),log(AGN$EW_H_alpha,10),
-                       log(AGN$OIII/AGN$H_beta,10))
-test_index <- sample(seq_len(nrow(AGN_high)),replace=F, size = 15000)
-AGN_short <- AGN_high[test_index,]
+AGN<- read.table("/Users/rafael/Dropbox/artigos/Meusartigos/IAA-WGC/Github/Gal_classification/Dataset/class_WHAN_BPT.dat",header=F)
+colnames(AGN)<-c("id", "xx_BPT", "yy_BPT", "class_BPT", "xx_WHAN",
+                         "yy_WHAN", "EW_NII_WHAN", "class_WHAN")
 
-write.csv(AGN_short,"AGN_short_sample.csv",row.names = F)
+write.csv(AGN,"class_WHAN_BPT.csv"),row.names=F)
 
-CLUST <- Mclust(AGN_short,G = 1:4)
+test_index <- sample(seq_len(nrow(AGN)),replace=F, size = 10000)
+AGN_short <- AGN[test_index,c("xx_BPT", "yy_BPT","yy_WHAN")]
+
+
+CLUST <- Mclust(AGN_short,G = 3)
 plot(CLUST)
 
 
-# 3D plot
 
-x <-  AGN_short[,1]
-y <-  AGN_short[,2]
-z <-  AGN_short[,3]
-
-require(car)
-require(scatterplot3d)
-scatter3d(x,y,z, groups = as.factor(CLUST$classification),
-          surface = FALSE)
-
-library(plotly)
-plot_ly(x = x, y = y, z = z, color  = as.factor(CLUST$classification),type = "scatter3d", mode = "markers") %>% 
-  layout(scene = list(
-           xaxis = list(title = "LogNII_Ha"), 
-           yaxis = list(title = "EWHa"), 
-           zaxis = list(title = "LogOIII_Hb")))  
-
-
-gdata <- data.frame(x=AGN_short[,1],y=AGN_short[,2],z=AGN_short[,3], type=as.factor(CLUST$classification))
+gdata <- data.frame(x=AGN_short$xx_BPT,y=AGN_short$yy_BPT,z=AGN_short$yy_WHAN, type=as.factor(CLUST$classification))
 
 #-----------------------
 # BPT PLOT
@@ -54,7 +38,7 @@ gSey <- data.frame(xx2,Sey)
 
 
 
-ggplot(data=gdata,aes(x=x,y=z))+geom_point(aes(color=type))+
+ggplot(data=gdata,aes(x=x,y=y))+geom_point(aes(color=type))+
   xlab(expression(paste('log ([NII]/H', alpha, ')'))) +
   ylab(expression(paste('log ([OIII]/H', beta, ')'))) +
   scale_colour_manual(values = c("#66c2a5","#fc8d62","#8da0cb","#e78ac3"))+
@@ -71,7 +55,7 @@ ggplot(data=gdata,aes(x=x,y=z))+geom_point(aes(color=type))+
 
 
 
-ggplot(data=gdata,aes(x=x,y=y))+geom_point(aes(color=type))+
+ggplot(data=gdata,aes(x=x,y=z))+geom_point(aes(color=type))+
   xlab(expression(paste('log ([NII]/H', alpha, ')'))) +
   ylab(expression(paste('log EW (H', alpha, ')'))) +
   scale_colour_manual(values = c("#66c2a5","#fc8d62","#8da0cb","#e78ac3","#fb9a99"))+
@@ -82,7 +66,50 @@ ggplot(data=gdata,aes(x=x,y=y))+geom_point(aes(color=type))+
         axis.title.x=element_text(vjust=-0.25),
         text = element_text(size=20))+
   geom_segment(aes(x = -0.4, y = 5, xend = -0.4, yend = 0.5),size=1.25,linetype="dashed",color="gray25")+
-  geom_hline(yintercept = 0.5,linetype="dashed",size=1.25,color="gray25")+
-geom_segment(aes(x = -0.4, y = 0.78, xend = 2, yend = 0.78),linetype="dashed",size=1.25,color="gray25")
-  
-  
+ geom_hline(yintercept = 0.5,linetype="dashed",size=1.25,color="gray25")+
+  geom_segment(aes(x = -0.4, y = 0.78, xend = 2, yend = 0.78),linetype="dashed",size=1.25,color="gray25")
+
+# Diagnostics
+
+S1<-silhouette(CLUST$classification,daisy(AGN_short))
+
+S_BPT<-silhouette(AGN[test_index,]$class_BPT,daisy(AGN_short))
+
+
+S_WHAN<-silhouette(AGN[test_index,]$class_WHAN,daisy(AGN_short))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3D plot
+
+x <-  AGN_short[,1]
+y <-  AGN_short[,2]
+z <-  AGN_short[,3]
+
+require(car)
+require(scatterplot3d)
+scatter3d(x,y,z, groups = as.factor(CLUST$classification),
+          surface = FALSE)
+
+library(plotly)
+plot_ly(x = x, y = y, z = z, color  = as.factor(CLUST$classification),type = "scatter3d", mode = "markers") %>% 
+  layout(scene = list(
+           xaxis = list(title = "LogNII_Ha"), 
+           yaxis = list(title = "EWHa"), 
+           zaxis = list(title = "LogOIII_Hb")))  
+
+
+
