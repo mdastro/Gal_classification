b0d966d4527c540e5a5f2c3e2d27882ea1e84d28
Rafael <rafael.2706@gmail.com>
2016-09-30 10:37:08 +0200

    mix

--- Rafael_R_scripts/GMM_WHAN_BPT_4.R
+++ Rafael_R_scripts/GMM_WHAN_BPT_4.R
@@ -28,10 +28,20 @@ CLUST <- Mclust(AGN_short,G = 3,initialization=list(subset=sample(1:nrow(AGN_sho
                 modelName = "VVV")#Initialization with 1000 for higher speed
 
 #--Get Ellipse info----------------------------------------------------------------#
-source("gg_ellipse.R")
-EL<-df.ellipses(CLUST,level=0.997)
-El_BPT<-subset(EL,xvar=="yy_BPT" & yvar=="xx_BPT")
-El_BPT$classification <-as.factor(El_BPT$classification)
+#source("gg_ellipse.R")
+EL68<-df.ellipses(CLUST,level=0.68)
+EL95<-df.ellipses(CLUST,level=0.95)
+EL99<-df.ellipses(CLUST,level=0.997)
+
+El_BPT68<-subset(EL68,xvar=="yy_BPT" & yvar=="xx_BPT")
+El_BPT68$classification <-as.factor(El_BPT68$classification)
+#
+El_BPT95<-subset(EL95,xvar=="yy_BPT" & yvar=="xx_BPT")
+El_BPT95$classification <-as.factor(El_BPT95$classification)
+#
+El_BPT99<-subset(EL99,xvar=="yy_BPT" & yvar=="xx_BPT")
+El_BPT99$classification <-as.factor(El_BPT99$classification)
+#
 
 El_WHAN<-subset(EL,xvar=="yy_WHAN" & yvar=="xx_BPT")
 El_WHAN$classification <-as.factor(El_WHAN$classification)
@@ -60,12 +70,20 @@ ggplot(data=gdata,aes(x=x,y=y))+
   xlab(expression(paste('log ([NII]/H', alpha, ')'))) +
   ylab(expression(paste('log ([OIII]/H', beta, ')'))) +
 #  stat_ellipse(type="norm",geom = "polygon", alpha = 1/2,aes(group=type,fill=type),level = 0.997)+
- 
-  geom_point(aes(color=type),size=0.25)+
+  geom_point(color="gray80",alpha=0.2,size=0.5)+
   scale_colour_manual(values = c("#66c2a5","#fc8d62","#8da0cb","#e78ac3"))+
   scale_fill_manual(values = c("#66c2a5","#fc8d62","#8da0cb","#e78ac3"))+
-  geom_path(data=El_BPT,aes(x=xval,y=yval,group=classification,color=classification),size=1)+
-  theme_bw() + 
+ # geom_path(data=El_BPT99,aes(x=xval,y=yval,group=classification,color=classification
+#                                 ),size=1)+
+  geom_polygon(data=El_BPT95,aes(x=xval,y=yval,group=classification,
+              color=classification,fill=classification),
+               size=1,alpha=0.2)+
+  
+  geom_polygon(data=El_BPT68,aes(x=xval,y=yval,group=classification,color=classification,
+                              fill=classification
+                                 ),size=1,alpha=0.4)+
+
+ theme_bw() + 
   geom_line(aes(x=xx,y=Ka),data=gKa,size=1.25,linetype="dashed",color="gray25")+
   geom_line(aes(x=xx1,y=Ke),data=gKe,size=1.25,linetype="dotted",color="gray25")+
   geom_line(aes(x=xx2,y=Sey),data=gSey,size=0.75,linetype="dotdash",color="gray25")+
