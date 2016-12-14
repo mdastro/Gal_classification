3fc8f45b37dc9fa1642ccf58ae2ee2f350fb23f2
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2016-12-13 23:24:40 -0200

    Update bpt_whan_seaborn.py

--- Malu_Python_Scripts/bpt_whan_seaborn.py
+++ Malu_Python_Scripts/bpt_whan_seaborn.py
@@ -3,11 +3,9 @@
 
 """
     This python program plots the BPT and WHAN diagrams using the SEABORN package.
-    This version: the dataset changed (including the format) and this program is now 
-    updated regarding the new format of the dataset.
     @author:  Maria Luiza Linhares Dantas
-    @date:    2016.27.08
-    @version: 0.0.2
+    @date:    2016.14.12
+    @version: 0.0.3
 """
 
 from __future__ import division
@@ -21,22 +19,22 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 
     # Configuring the inputs -------------------------------------------------------------------------------------------
-    my_data = np.loadtxt('/home/mldantas/Dropbox/Clustering/clean_data_lines.csv', delimiter=',', dtype=str)
+    my_data = np.loadtxt('/home/mldantas/Dropbox/Clustering/Dataset/results_classification.csv', delimiter=',', dtype=str)
 
     my_dictionary = {}
     for i in range(len(my_data[0, :])):                                         # Converting numpy array into dictionary
         my_dictionary[my_data[0, i]] = np.array(my_data[0 + 1:, i], dtype=str)
 
-    bpt_x  = my_dictionary['xx_BPT'].astype(float)
-    bpt_y  = my_dictionary['yy_BPT'].astype(float)
-    whan_x = my_dictionary['xx_WHAN'].astype(float)
-    whan_y = my_dictionary['yy_WHAN'].astype(float)
+    # bpt_x  = my_dictionary['xx_BPT'].astype(float)
+    # bpt_y  = my_dictionary['yy_BPT'].astype(float)
+    # whan_x = my_dictionary['xx_WHAN'].astype(float)
+    # whan_y = my_dictionary['yy_WHAN'].astype(float)
 
-    # my_nii        = my_dictionary['NII'].astype(float)
-    # my_oiii       = my_dictionary['OIII'].astype(float)
-    # my_h_alpha    = my_dictionary['H_alpha'].astype(float)
-    # my_h_beta     = my_dictionary['H_beta'].astype(float)
-    # my_ew_h_alpha = my_dictionary['EW_H_alpha'].astype(float)
+    my_nii        = my_dictionary['NII'].astype(float)
+    my_oiii       = my_dictionary['OIII'].astype(float)
+    my_h_alpha    = my_dictionary['H_alpha'].astype(float)
+    my_h_beta     = my_dictionary['H_beta'].astype(float)
+    my_ew_h_alpha = my_dictionary['EW_H_alpha'].astype(float)
 
     ## BPT -------------------------------------------------------------------------------------------------------------
     xbpt_01 = np.linspace(-1.8, -0.1, 1000)
@@ -69,60 +67,55 @@ if __name__ == '__main__':
     schawinski_y = np.array(schawinski_y)
 
     # Plots ------------------------------------------------------------------------------------------------------------
-    index = np.where(np.log10(bpt_x) != 0)
+    index = np.where(np.log10(my_nii/my_h_alpha) != 0)
 
-    # my_cool_palette = ['#00CED1', '#191970', '#FFA500', '#A020F0']
-    # sns.palplot(sns.color_palette(my_cool_palette))
-    plt.subplot(1, 2, 1)
-    sns.set_style("darkgrid")
-    # sns.palplot(sns.color_palette(my_cool_palette))
-    # plot01  = plt.scatter(np.log10(my_nii/my_h_alpha)[index], np.log10(my_oiii/my_h_beta)[index], c='gray', alpha=0.2)
-    cmap = sns.cubehelix_palette(8, rot=-.75, as_cmap=True)
-    sns.kdeplot(bpt_x[index], bpt_y[index], cmap=cmap, shade=True, cut=5)
-    plot02, = plt.plot(xbpt_01, ybpt_01, '-.', color='#FF4500', linewidth=3.)
-    plot03, = plt.plot(xbpt_02, ybpt_02, '-', color='#1E90FF', linewidth=3.)
-    plot04, = plt.plot(schawinski_x, schawinski_y, '-', color='#FF1493', linewidth=3.)
-    plot05, = plt.plot(xbpt_03, ybpt_03, '--', color='#6B8E23', linewidth=3.)
-    plt.legend([plot02, plot03, plot04, plot05], [r"""$\rmKauffman+03$""", r"""$\rmKewley+01$""",
-                                                  r"""$\rmSchawinski+07$""", r"""$\rm Stasi\' nska+06$"""], numpoints=1,
-               loc='lower left', fontsize=20)
-    plt.xlabel(r"$\log ([NII]/H{\alpha})$", fontweight='bold', fontsize=30)
-    plt.ylabel(r"$\log (\left[OIII\right]/H \beta) $", fontweight='bold', fontsize=30)
-    plt.text(-1.3, -0.4, r"Star Forming", fontsize=20)
-    plt.text(-0.8, 1.0, r"AGN", fontsize=20)
-    plt.text(-0.8, 0.9, r"Seyfert", fontsize=20)
-    plt.text(0.3, 0.5, r"AGN(?)", fontsize=20)
-    plt.text(0.3, 0.4, r"LI(N)ER", fontsize=17)
-    plt.text(-0.25, -1.1, r"Composite", fontsize=20)
-    plt.xlim([-1.8, 1.2])
-    plt.ylim([-1.3, 1.55])
-    plt.minorticks_on()
-    plt.tick_params('both', labelsize='30')
-    plt.grid(alpha=0.5)
-    plt.subplots_adjust(top=0.95, bottom=0.15, left=0.10, right=0.60)
-    # plt.show()
+    with sns.axes_style('white', {'axes.grid': False}):
 
-    plt.subplot(1, 2, 2)
-    # sns.set_style("darkgrid")
-    # sns.palplot(sns.color_palette(my_cool_palette))
-    # plot01  = plt.scatter(np.log10(my_nii / my_h_alpha)[index], np.log10(my_ew_h_alpha)[index], c='gray', alpha=0.2)
-    plot02, = plt.plot([-0.4, -0.4], [3.5, 0.5], '-', color='#FF1493', linewidth=3.)
-    plot03, = plt.plot([-0.4, 3.5], [0.78, 0.78], '-', color='#FF1493', linewidth=3.)
-    cmap = sns.cubehelix_palette(8, rot=-.75, as_cmap=True)
-    sns.kdeplot(whan_x[index], whan_y[index], cmap=cmap, shade=True, cut=5)
-    # plt.legend([plot01], [r"$\propto \, D_{n}4000_{synth}$ break"], numpoints=1, loc='lower left', fontsize=20)
-    plt.axhline(y=+0.5, color='#FF1493', linewidth=2.)
-    plt.xlabel(r"$\log ([NII]/H{\alpha})$", fontweight='bold', fontsize=30)
-    plt.ylabel(r"$\log EW(H{\alpha})$", fontweight='bold', fontsize=30)
-    plt.text(-1.0, 0., r"Retired/Passive", fontsize=20)
-    plt.text(0.35, 1.0, r"sAGN", fontsize=20)
-    plt.text(0.35, 0.6, r"wAGN", fontsize=20)
-    plt.text(-1.5, 0.6, r"Star Forming", fontsize=20)
-    plt.xlim([-1.8, 1.2])
-    plt.ylim([-1.1, 2.5])
-    plt.minorticks_on()
-    plt.tick_params('both', labelsize='30')
-    plt.grid(alpha=0.5)
-    plt.subplots_adjust(top=0.95, bottom=0.15, left=0.10, right=0.60)
+        plt.subplot(1, 2, 1)
+        # sns.palplot(sns.color_palette(my_cool_palette))
+        # plot01  = plt.scatter(np.log10(my_nii/my_h_alpha)[index], np.log10(my_oiii/my_h_beta)[index], c='gray', alpha=0.2)
+        sns.kdeplot(np.log10(my_nii/my_h_alpha)[index], np.log10(my_oiii/my_h_beta)[index])
+        plot02, = plt.plot(xbpt_01, ybpt_01, '-.', color='black', linewidth=3., alpha=0.75)
+        plot03, = plt.plot(xbpt_02, ybpt_02, '.', color='black', linewidth=3., alpha=0.75)
+        plot04, = plt.plot(schawinski_x, schawinski_y, '-', color='black', linewidth=3., alpha=0.75)
+        plot05, = plt.plot(xbpt_03, ybpt_03, '--', color='black', linewidth=3.)
+        plt.legend([plot02, plot03, plot04, plot05], [r"""$\rmKauffman+03$""", r"""$\rmKewley+01$""",
+                                                      r"""$\rmSchawinski+07$""", r"""$\rm Stasi\' nska+06$"""], numpoints=1,
+                   loc='lower left', fontsize=17)
+        plt.xlabel(r"$\log ([NII]/H{\alpha})$", fontweight='bold', fontsize=30)
+        plt.ylabel(r"$\log (\left[OIII\right]/H \beta) $", fontweight='bold', fontsize=30)
+        plt.text(-1.6, -0.2, r"Star Forming", fontsize=18)
+        plt.text(-0.8, 1.0, r"AGN", fontsize=18)
+        plt.text(-0.8, 0.8, r"Seyfert", fontsize=18)
+        plt.text(0.3, 0.5, r"AGN(?)", fontsize=18)
+        plt.text(0.3, 0.3, r"LI(N)ER", fontsize=18)
+        plt.text(-0.36, -1.1, r"Composite", fontsize=18)
+        plt.xlim([-1.8, 1.2])
+        plt.ylim([-1.3, 1.55])
+        # plt.minorticks_on()
+        plt.tick_params('both', labelsize='25')
+        # plt.grid(alpha=0.5)
 
+        plt.subplot(1, 2, 2)
+        # plot01  = plt.scatter(np.log10(my_nii / my_h_alpha)[index], np.log10(my_ew_h_alpha)[index], c='gray', alpha=0.2)
+        plot02, = plt.plot([-0.4, -0.4], [3.5, 0.5], '-', color='black', linewidth=3.)
+        plot03, = plt.plot([-0.4, 3.5], [0.78, 0.78], '-', color='black', linewidth=3.)
+        sns.kdeplot(np.log10(my_nii / my_h_alpha)[index], np.log10(my_ew_h_alpha)[index])
+        # plt.legend([plot01], [r"$\propto \, D_{n}4000_{synth}$ break"], numpoints=1, loc='lower left', fontsize=18)
+        plt.axhline(y=+0.5, color='black', linewidth=2.)
+        plt.xlabel(r"$\log ([NII]/H{\alpha})$", fontweight='bold', fontsize=30)
+        plt.text(-1.0, 0., r"Retired/Passive", fontsize=18)
+        plt.ylabel(r"$\log EW(H{\alpha})$", fontweight='bold', fontsize=30)
+        plt.text(0.35, 1.0, r"sAGN", fontsize=18)
+        plt.text(0.35, 0.55, r"wAGN", fontsize=18)
+        plt.text(-1.4, 0.55, r"Star Forming", fontsize=18)
+        plt.xlim([-1.8, 1.2])
+        plt.ylim([-1.1, 2.5])
+        # plt.minorticks_on()
+        plt.tick_params('both', labelsize='25')
+        # plt.grid(alpha=0.5)
+        # plt.subplots_adjust(top=0.95, bottom=0.15, left=0.10, right=0.60)
+
+    plt.subplots_adjust(left=0.09, bottom=0.30, right=0.99, top=0.93, wspace=0.25, hspace=0.20)
     plt.show()
+
